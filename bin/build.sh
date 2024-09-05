8ce870978a582be201d3060fe53602678e61ed26
Jeferson Silva <jeferson.i.silva@gmail.com>
2024-09-05 03:40:16 -0300

    build

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-rm -rf ./docs
+rm -rf ./docs && mkdir ./docs
 docker run --rm -it -v ${PWD}/nuxt3:/app -w /app node:18 yarn generate
-sudo chmod 0777 -R ./nuxt3/.output/public
-mv ./nuxt3/.output/public ./docs/
+cp -rv ./nuxt3/.output/public/* ./docs/
